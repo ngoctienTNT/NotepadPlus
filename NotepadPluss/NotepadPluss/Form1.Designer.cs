59558ed6152520696cee06c903c085fce3c2a3dc
Ngọc Tiến Trần <ngoctienTNT>
2021-10-11 19:15:12 +0700

    TNT

---
+++ NotepadPluss/NotepadPluss/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace NotepadPluss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rTbMain = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.formatToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1262, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 26);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Image = global::NotepadPluss.Properties.Resources.new_file;
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Image = global::NotepadPluss.Properties.Resources.open_folder;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Image = global::NotepadPluss.Properties.Resources.floppy_disk;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Image = global::NotepadPluss.Properties.Resources.save_file;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Image = global::NotepadPluss.Properties.Resources.exit;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontToolStripMenuItem,
+            this.textColorToolStripMenuItem,
+            this.backgroundColorToolStripMenuItem});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(70, 26);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Image = global::NotepadPluss.Properties.Resources.font_size;
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(262, 26);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // textColorToolStripMenuItem
+            // 
+            this.textColorToolStripMenuItem.Image = global::NotepadPluss.Properties.Resources.text;
+            this.textColorToolStripMenuItem.Name = "textColorToolStripMenuItem";
+            this.textColorToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.T)));
+            this.textColorToolStripMenuItem.Size = new System.Drawing.Size(262, 26);
+            this.textColorToolStripMenuItem.Text = "Text Color";
+            this.textColorToolStripMenuItem.Click += new System.EventHandler(this.textColorToolStripMenuItem_Click);
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            this.backgroundColorToolStripMenuItem.Image = global::NotepadPluss.Properties.Resources.theme;
+            this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            this.backgroundColorToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+            this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(262, 26);
+            this.backgroundColorToolStripMenuItem.Text = "Background Color";
+            this.backgroundColorToolStripMenuItem.Click += new System.EventHandler(this.backgroundColorToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // rTbMain
+            // 
+            this.rTbMain.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rTbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rTbMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rTbMain.Location = new System.Drawing.Point(0, 30);
+            this.rTbMain.Margin = new System.Windows.Forms.Padding(5);
+            this.rTbMain.Name = "rTbMain";
+            this.rTbMain.Size = new System.Drawing.Size(1262, 643);
+            this.rTbMain.TabIndex = 1;
+            this.rTbMain.Text = "";
+            this.rTbMain.TextChanged += new System.EventHandler(this.rTbMain_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.rTbMain);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New Notepad Plus - TNT";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox rTbMain;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColorToolStripMenuItem;
+    }
+}
+
---
+++ NotepadPluss/NotepadPluss/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace NotepadPluss
+{
+    public partial class Form1 : Form
+    {
+        private string fileName;
+        private bool check = true;
+        public Form1()
+        {
+            InitializeComponent();
+            fileName = "";
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Lập trình bởi Trần Ngọc Tiến\nPhiên bản 1.0.0", "About Notepad Plus", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool control = true;
+            if ((fileName == "" && rTbMain.Text != "") || (fileName != "" && check == false))
+            {
+                DialogResult result;
+                result = MessageBox.Show("Bạn có muốn lưu nội dung lại không?", "Thông báo", MessageBoxButtons.YesNoCancel, MessageBoxIcon.None);
+                if (result == DialogResult.Yes)
+                {
+                    if (fileName == "")
+                    {
+                        FileDialog dialog = new SaveFileDialog();
+                        dialog.Filter = "Text|*.txt";
+                        if (dialog.ShowDialog() == DialogResult.OK)
+                        {
+                            StreamWriter writer = new StreamWriter(dialog.FileName);
+                            writer.Write(rTbMain.Text);
+                            writer.Close();
+                        }
+                    }
+                    else
+                    {
+                        StreamWriter writer = new StreamWriter(fileName);
+                        writer.WriteLine(rTbMain.Text);
+                        writer.Close();
+                    }
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    control = false;
+                }
+            }
+            if (control == false) return;
+            FileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Text|*.txt";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = fileDialog.FileName;
+                this.Text = Path.GetFileName(fileName) + " - Notepad Plus";
+                StreamReader reader = new StreamReader(fileDialog.FileName);
+                rTbMain.Text = reader.ReadToEnd();
+                reader.Close();
+            }
+            check = true;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {            
+            if (fileName == "") 
+            {
+                FileDialog fileDialog = new SaveFileDialog();
+                fileDialog.Filter = "Text|*.txt";
+                if (fileDialog.ShowDialog()==DialogResult.OK)
+                {
+                    StreamWriter writer = new StreamWriter(fileDialog.FileName);
+                    writer.Write(rTbMain.Text);
+                    writer.Close();
+                    fileName = fileDialog.FileName;
+                    this.Text = Path.GetFileName(fileName) + " - Notepad Plus";
+                }    
+            }    
+            else
+            {
+                StreamWriter writer = new StreamWriter(fileName);
+                writer.WriteLine(rTbMain.Text);
+                writer.Close();
+            }
+            check = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileDialog fileDialog = new SaveFileDialog();
+            fileDialog.Filter = "Text|*.txt";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(fileDialog.FileName);
+                writer.Write(rTbMain.Text);
+                writer.Close();
+                fileName = fileDialog.FileName;
+                this.Text = Path.GetFileName(fileName) + " - Notepad Plus";
+            }
+            check = true;
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {    
+            if ((fileName == "" && rTbMain.Text != "") || (fileName != "" && check == false))
+            {
+                bool kt = false;
+                DialogResult result;
+                result = MessageBox.Show("Bạn có muốn lưu nội dung lại không?", "Thông báo", MessageBoxButtons.YesNoCancel, MessageBoxIcon.None);
+                if (result == DialogResult.Yes)
+                {
+                    if (fileName == "")
+                    {
+                        FileDialog fileDialog = new SaveFileDialog();
+                        fileDialog.Filter = "Text|*.txt";
+                        if (fileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            StreamWriter writer = new StreamWriter(fileDialog.FileName);
+                            writer.Write(rTbMain.Text);
+                            writer.Close();
+                            kt = true;
+                        }
+                    }
+                    else
+                    {
+                        StreamWriter writer = new StreamWriter(fileName);
+                        writer.WriteLine(rTbMain.Text);
+                        writer.Close();
+                        kt = true;
+                    }
+                    if (kt)
+                    {
+                        check = true;
+                        rTbMain.Text = "";
+                        fileName = "";
+                        this.Text = "New Notepad Plus - TNT";
+                    }
+                }
+                else if (result == DialogResult.No)
+                {
+                    rTbMain.Text = "";
+                    fileName = "";
+                    this.Text = "New Notepad Plus - TNT";
+
+                }
+            }  
+            else
+            {
+                rTbMain.Text = "";
+                fileName = "";
+                this.Text = "New Notepad Plus - TNT";
+                check = true;
+            }    
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if ((fileName == "" && rTbMain.Text != "") || (fileName != "" && check == false))
+            {
+                bool kt = false;
+                DialogResult result;
+                result = MessageBox.Show("Bạn có muốn lưu nội dung lại không?", "Thông báo", MessageBoxButtons.YesNoCancel, MessageBoxIcon.None);
+                if (result == DialogResult.Yes)
+                {
+                    if (fileName == "")
+                    {
+                        FileDialog fileDialog = new SaveFileDialog();
+                        fileDialog.Filter = "Text|*.txt";
+                        if (fileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            StreamWriter writer = new StreamWriter(fileDialog.FileName);
+                            writer.Write(rTbMain.Text);
+                            writer.Close();
+                            kt = true;
+                        }
+                    }
+                    else
+                    {
+                        StreamWriter writer = new StreamWriter(fileName);
+                        writer.WriteLine(rTbMain.Text);
+                        writer.Close();
+                        kt = true;
+                    }
+                    if (kt == false) e.Cancel = true;
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void rTbMain_TextChanged(object sender, EventArgs e)
+        {
+            check = false;
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK) rTbMain.Font = fontDialog.Font;
+        }
+
+        private void textColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK) rTbMain.ForeColor = colorDialog.Color;
+        }
+
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK) rTbMain.BackColor = colorDialog.Color;
+        }
+    }
+}
